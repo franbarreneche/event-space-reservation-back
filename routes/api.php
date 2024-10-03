316f990d2acc72b6e2932e1c414473f35bda3175
franbarreneche <franbarreneche@gmail.com>
2024-10-02 22:09:30 -0300

    feat: adds auth routes and logic

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('auth')
+    ->middleware('auth:api')
+    ->controller(\App\Http\Controllers\AuthController::class)
+    ->group(function() {
+        Route::post('login','login')->withoutMiddleware('auth:api');
+        Route::post('logout','logout');
+        Route::post('refresh','refresh');
+        Route::post('me','me');
 });
